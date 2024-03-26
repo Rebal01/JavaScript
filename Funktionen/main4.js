241ba2c9280e07733ede4d61cd057b91188a5873
Saad Shekhani <sadomr425@gmail.com>
2024-03-26 09:32:47 +0100

    Finish main4.

--- Funktionen/main4.js
+++ Funktionen/main4.js
@@ -1,8 +1,8 @@
-// function log(...args) {
-//     let text = "";
-//     for (let i = 0; i < args.length; i++) {
-//         text += args[i] + ' ';
-//     }
-//     console.log(text);
-// }
-
+function log(...args) {
+    let text = "";
+    for (let i = 0; i < args.length; i++) {
+        text += args[i] + ' ';
+    }
+    console.log(text);
+}
+log("hello",true,456456)

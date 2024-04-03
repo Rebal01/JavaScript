49fbfa9ca7092d29599ce587c308e3c3018ff5a3
Saad Shekhani <sadomr425@gmail.com>
2024-04-03 13:33:38 +0200

    I deleted a word "Idiot"

--- ArraysObjekte/main1.js
+++ ArraysObjekte/main1.js
@@ -4,17 +4,17 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function execute() {
-    const arr = ["","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-	const user = Number(await prompt("Enter a number from 1 to 7 to find out the days of the week: "));
-            if (isNaN(user) ) {
-                console.log("The input you entered is incorrect. ");
-            return ;
-            }
-            else if (user <=0 || user>7) {
-                console.log(`This is not a day of the week, you idiot ${user}`);
-                return ;
-            }
-            console.log(arr[user]);
+    const arr = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const user = Number(await prompt("Enter a number from 1 to 7 to find out the days of the week: "));
+    if (isNaN(user)) {
+        console.log("The input you entered is incorrect. ");
+        return;
+    }
+    else if (user <= 0 || user > 7) {
+        console.log(`This is not a day of the week${user}`);
+        return;
+    }
+    console.log(arr[user]);
 }
 
 execute().catch((err) => { console.error(err); }).finally(() => rl.close());

34eafc7bd89f88eafdc0c27ef406a34ad0a8a621
Saad Shekhani <sadomr425@gmail.com>
2024-04-03 11:05:01 +0200

    FIX MAIN 5

--- Verzweigungen/main5.js
+++ Verzweigungen/main5.js
@@ -4,39 +4,41 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function execute() {
-	const user =  await prompt("Enter one letter from A to Z: ");
-    switch(user){
-        case 'a':
+    let user = (await prompt("Enter one letter from a to z: ")).toLowerCase();
+    switch (user) {
         case 'b':
         case 'c':
         case 'd':
-        case 'e':
         case 'f':
         case 'g':
         case 'h':
-        case 'i':
         case 'g':
         case 'k':
         case 'l':
         case 'm':
         case 'n':
-        case 'o':
         case 'p':
         case 'q':
         case 'r':
         case 's':
         case 't':
-        case 'u':
         case 'v':
         case 'w':
         case 'x':
         case 'w':
         case 'z':
-        console.log("That's right, it's a letter. Good job, boy",user);
-        break;
+            console.log(`That's right, it's a Consonant letter. Good job, boy, ${user}`);
+            break;
+        case 'a':
+        case 'e':
+        case 'u':
+        case 'o':
+        case 'i':
+            console.log(`That's right, It's a vowel letter. Good job, boy, ${user}`);
+
         default:
-        console.log("This is not a letter",user," idiot. You must learn the letters");
-        break;
+            console.log(`This is not a letter, ${user},. You must learn the letters`);
+            break;
     }
 }
 

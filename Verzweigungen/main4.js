6ed4f6493bb094b94a547c96b1570becb73c33f2
Saad Shekhani <sadomr425@gmail.com>
2024-04-03 10:51:54 +0200

    FIX MAIN 4

--- Verzweigungen/main4.js
+++ Verzweigungen/main4.js
@@ -4,32 +4,32 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function execute() {
-	const user = await prompt("Enter a number from 1 to 7 to find out the days of the week: ");
-    switch(user){
-        case 1:
+    const user = await prompt("Enter a number from 1 to 7 to find out the days of the week: ");
+    switch (user) {
+        case '1':
             console.log(`Today is Monday ${user}`);
-        break;
-        case 2:
+            break;
+        case '2':
             console.log(`Today is Tuesday ${user}`);
-        break;
-        case 3:
+            break;
+        case '3':
             console.log(`Today is Wednesday ${user}`);
-        break;
-        case 4:
+            break;
+        case '4':
             console.log(`Today is Thursday ${user}`);
-        break;
-        case 5:
+            break;
+        case '5':
             console.log(`Today is Friday ${user}`);
-        break;
-        case 6:
+            break;
+        case '6':
             console.log(`Today is Saturday ${user}`);
-        break;
-        case 7:
+            break;
+        case '7':
             console.log(`Today is Sunday ${user}`);
-        break;
+            break;
         default:
-        console.log(`This is not a day of the week, you idiot ${user}`);
-        break;
+            console.log(`This is not a day of the week, you idiot ${user}`);
+            break;
     }
 }
 

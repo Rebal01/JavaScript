7dc3350d10423f4726318a8b924816a150102d27
Saad Shekhani <118700092+Rebal01@users.noreply.github.com>
2024-04-08 09:57:47 +0000

    logger

---
+++ npm/logger.mjs
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+export function logRed(red) {
+    console.log(chalk.red(`${red}`));
+}
+export function logGreen(green) {
+    console.log(chalk.green(`${green}`));
+}
+export function logBlue(blue) {
+    console.log(chalk.blue(`${blue}`));
+}

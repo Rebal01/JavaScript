8aaa0973023b823bb39687dbf0abebca3526d431
Saad Shekhani <118700092+Rebal01@users.noreply.github.com>
2024-04-09 09:11:03 +0000

    Fix main2 main3 Funktionen

--- Funktionen/main2.js
+++ Funktionen/main2.js
@@ -1,14 +1,7 @@
-const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-rl.on('close', () => process.exit(0));
-const isEven =(Number) => Number % 2 === 0 ? 'This is Even number' : 'This is odd number';
-async function execute() {
-	const user = Number(await prompt("Put your number: "));
-            if (isNaN(user) ) {
-                console.log("The input you entered is incorrect. ");
-            return ;
-            }
-            console.log(isEven(user))
+const isEven = (Number) => Number % 2 === 0 ? 'true' : 'false';
+const user = 2;
+if (isNaN(user)) {
+    console.log("The input you entered is incorrect. ");
+    return;
 }
-execute().catch((err) => { console.error(err); }).finally(() => rl.close());
+console.log(isEven(user))
